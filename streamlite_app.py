ca3e24da2f22b009421845b824f186586de63d73
pritampawar1 <125981533+pritampawar1@users.noreply.github.com>
2023-02-21 13:15:10 +0000

    Update streamlite_app.py
    added data from csv file using pandas and dataframe to display it

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,8 +1,10 @@
 import streamlit
-
+import pandas
 streamlit.title('My parents new healthy dinner')
 streamlit.header('🥣Breakfast menu')
 streamlit.text('🥑Omega 3 and Blueberry oatmeal')
 streamlit.text('🥗Kale spinacha and rocket smothie')
 streamlit.text('🐔Hard boiled free range eggs')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
