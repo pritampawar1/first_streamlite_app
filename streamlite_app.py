8ae3e3795bc9270c9ccda803e4f32f07e0603f4b
pritampawar1 <125981533+pritampawar1@users.noreply.github.com>
2023-02-21 13:09:38 +0000

    Update streamlite_app.py
    added Emojis

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 and Blueberry oatmeal')
-streamlit.text('Kale spinacha and rocket smothie')
-streamlit.text('Hard boiled free range eggs')
+streamlit.header('🥣Breakfast menu')
+streamlit.text('🥑Omega 3 and Blueberry oatmeal')
+streamlit.text('🥗Kale spinacha and rocket smothie')
+streamlit.text('🐔Hard boiled free range eggs')
