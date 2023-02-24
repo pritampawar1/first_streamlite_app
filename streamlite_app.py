3f3596b9ed4a061cc9397ff15cd3dbf3bff155d8
pritampawar1 <125981533+pritampawar1@users.noreply.github.com>
2023-02-24 17:35:44 +0000

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -45,6 +45,7 @@ except URLError as e:
     
 # show table on app
 #streamlit.dataframe(fruityvice_normalized)
+
 def get_fruit_load_list():
      with my_cnx.cursor() as my_cur:
             my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
@@ -62,9 +63,9 @@ if streamlit.button('Get fruit load list'):
     streamlit.dataframe(my_data_row)
 
  def insert_row_into_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur:
-            my_cur.execute("Insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values ('from streamlite')")
-            return "Thanks for adding new fruit" + new_fruit
+        with my_cnx.cursor() as my_cur:
+                my_cur.execute("Insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values ('from streamlite')")
+                return "Thanks for adding new fruit" + new_fruit
  
 fruit_choice = streamlit.text_input('What fruit would you like to add?')
         
