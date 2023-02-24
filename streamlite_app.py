ff65ef9445eb2ed8b552c6a0c77fda3fc8de1f47
pritampawar1 <125981533+pritampawar1@users.noreply.github.com>
2023-02-24 16:53:20 +0000

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -46,7 +46,7 @@ except URLError as e:
 # show table on app
 #streamlit.dataframe(fruityvice_normalized)
 def get_fruit_load_list():
-     with my_cur.cursor() as my_cur:
+     with my_cnx.cursor() as my_cur:
             my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
             return my_cur.fetchall()
             
