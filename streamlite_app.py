3318eeaf4ce6e7e408f40275975cc365d8715583
pritampawar1 <125981533+pritampawar1@users.noreply.github.com>
2023-02-24 15:27:56 +0000

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -27,15 +27,15 @@ streamlit.header("Fruityvice Fruit Advice!")
 # convert response to table
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
- if not fruit_choice:
-    streamlite.error("Please select fruit to get information")
-Else:
-  streamlit.write('The user entered ', fruit_choice)
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  if not fruit_choice:
+      streamlite.error("Please select fruit to get information")
+  Else:
+    streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   
- except URLError as e:
-  streamlit.error()
+  except URLError as e:
+    streamlit.error()
 # show table on app
 streamlit.dataframe(fruityvice_normalized)
 
