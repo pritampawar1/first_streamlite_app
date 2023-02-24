7804d3b4787cdb0e6204c63b28b3c420ef47b190
pritampawar1 <125981533+pritampawar1@users.noreply.github.com>
2023-02-24 12:48:40 +0000

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -41,5 +41,6 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 streamlit.header("Fruitload list contains")
 streamlit.dataframe(my_data_row)
-
+fruit_like_by_user = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('The user entered ', fruit_like_by_user)
 
